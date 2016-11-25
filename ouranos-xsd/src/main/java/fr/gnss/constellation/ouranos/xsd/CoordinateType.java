4d6f274320c44fffc22fe6b1b643f26c5ef1624d
Unknown <eric.langenbronn@gmail.com>
2016-11-25 20:13:48 +0100

    Correction pom livraison

--- ouranos-xsd/src/main/java/fr/gnss/constellation/ouranos/xsd/CoordinateType.java
+++ ouranos-xsd/src/main/java/fr/gnss/constellation/ouranos/xsd/CoordinateType.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.11 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2016.11.19 à 05:09:04 AM CET 
+// Généré le : 2016.11.25 à 08:11:21 PM CET 
 //
 
 
@@ -31,7 +31,7 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlType(name = "coordinateType", namespace = "http://www.ouranos.fr/ouranos/data/v10/types")
 @XmlEnum
-@Generated(value = "com.sun.tools.xjc.Driver", date = "2016-11-19T05:09:04+01:00", comments = "JAXB RI v2.2.11")
+@Generated(value = "com.sun.tools.xjc.Driver", date = "2016-11-25T08:11:21+01:00", comments = "JAXB RI v2.2.11")
 public enum CoordinateType {
 
     @XmlEnumValue("Cartesian")
