ce6813162736ea382d6129e46a882203320659ae
EricLangenbronn <eric.langenbronn@gmail.com>
2017-05-13 01:06:11 +0200

    correction bug + ajout trace en cas d'exception

--- ouranos-services/src/main/java/fr/gnss/constellation/ouranos/aspect/TraceInvocation.java
+++ ouranos-services/src/main/java/fr/gnss/constellation/ouranos/aspect/TraceInvocation.java
@@ -12,34 +12,6 @@ public class TraceInvocation implements Ordered {
 	private static Logger LOGGER = LoggerFactory.getLogger("OURANOS_TRACE_INVOCATION");
 	private int order;
 
-	public Object afficherTrace(final ProceedingJoinPoint joinpoint) throws Throwable {
-		String nomMethode = joinpoint.getTarget().getClass().getSimpleName() + "." + joinpoint.getSignature().getName();
-
-		final Object[] args = joinpoint.getArgs();
-		final StringBuffer sb = new StringBuffer();
-		sb.append(joinpoint.getSignature().toString());
-		sb.append(" avec les parametres : (");
-
-		for (int i = 0; i < args.length; i++) {
-			sb.append(args[i]);
-			if (i < args.length - 1) {
-				sb.append(", ");
-			}
-		}
-		sb.append(")");
-
-		LOGGER.info("Debut methode : " + sb);
-
-		Object obj = null;
-		try {
-			obj = joinpoint.proceed();
-		} finally {
-			LOGGER.info("Fin methode :  " + nomMethode + " retour=" + obj);
-		}
-
-		return obj;
-	}
-
 	public void afficherDebutTrace(final JoinPoint joinpoint) throws Throwable {
 		final Object[] args = joinpoint.getArgs();
 		final StringBuffer sb = new StringBuffer();
@@ -60,6 +32,11 @@ public class TraceInvocation implements Ordered {
 	public void afficherFinTrace(final StaticPart staticPart, final Object result) throws Throwable {
 		String nomMethode = staticPart.getSignature().toLongString();
 		LOGGER.info("Fin methode :  " + nomMethode + " retour=" + result);
+	}
+
+	public void afficherExceptionTrace(final StaticPart staticPart, final Exception exception) throws Throwable {
+		String nomMethode = staticPart.getSignature().toLongString();
+		LOGGER.error("Exception durant la methode :  " + nomMethode, exception);
 	}
 
 	@Override
